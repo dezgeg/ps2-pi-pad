0987faea9f87d6ea2252d8d1fbed8ab3f71ba3e0
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2023-03-31 20:07:37 +0300

    Outputting data works!

--- kmod/ps2pipad_kmod.c
+++ kmod/ps2pipad_kmod.c
@@ -43,9 +43,23 @@ static void dbgprintf(const char* format, ...) {
 }
 
 // GPIO FIQ stuff
+
+#define PIN_ACK (1u << 7)
+#define PIN_DAT (1u << 8)
+#define PIN_CLK (1u << 9)
+#define PIN_CMD (1u << 11)
+#define PIN_ATN (1u << 25)
+
+#define FSEL_ACK_MASK (0x7 << (3 * 7))
+#define FSEL_ACK_OUT  (0x1 << (3 * 7))
+#define FSEL_DAT_MASK (0x7 << (3 * 8))
+#define FSEL_DAT_OUT  (0x1 << (3 * 8))
+
 #define GPIO_BASE 0x20200000
-#define GPIO_GPLEV0 0x34 /* Pin Level */
-#define GPIO_GPEDS0 0x40 /* Pin Event Detect Status */
+#define GPIO_GPFSEL0 0x00 /* Function Select */
+#define GPIO_GPCLR0  0x28 /* Output Clear */
+#define GPIO_GPLEV0  0x34 /* Pin Level */
+#define GPIO_GPEDS0  0x40 /* Pin Event Detect Status */
 
 static void __iomem* gpio_base;
 
@@ -55,12 +69,6 @@ static struct fiq_handler fh = {
 static unsigned long fiq_stack[1024];
 
 extern char ps2pipad_fiq, ps2pipad_fiq_end;
-
-#define PIN_ACK (1u << 7)
-#define PIN_DAT (1u << 8)
-#define PIN_CLK (1u << 9)
-#define PIN_CMD (1u << 11)
-#define PIN_ATN (1u << 25)
 
 static u32 prev_fiq_start;
 static u32 prev_fiq_end;
@@ -84,10 +92,8 @@ static uint32_t new_mode = MODE_DIGITAL;
 static bool new_in_config = false;
 
 // Verifier
-static uint8_t expected_dat_resp[256];
-static bool compare_dat_resp = false;
+static uint8_t dat_resp[256];
 static bool check_in_config = false;
-static bool known_command = false;
 
 #define CHECK(val, exp) do_check(val, exp, #val)
 static void do_check(uint32_t value, uint32_t expected, const char* what) {
@@ -106,80 +112,75 @@ static const u8 CMD_4F_RESP[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x5A };
 #define CONFIG_RESP_SIZE 6
 
 static void got_byte(size_t index) {
-    // Static field checks
-    if (index == 0) {
-        // Initialize
-        compare_dat_resp = false;
-        check_in_config = false;
-        known_command = false;
-
-        CHECK(cmd_bytes[0], 0x01);
-        CHECK(dat_bytes[0], 0xff);
-        return;
-    }
-
-    // if (index == 2) {
-        // CHECK(cmd_bytes[2], 0x00);
-        // CHECK(dat_bytes[2], 0x5a);
-        // return;
-    // }
-
     // Commands without any data dependencies on parameters
     uint32_t cmd = cmd_bytes[1];
     if (index == 1) {
         switch (cmd) {
             case 0x40: {
-                compare_dat_resp = true;
                 check_in_config = true;
-                known_command = true;
-                memcpy(expected_dat_resp, CMD_40_RESP, CONFIG_RESP_SIZE);
+                memcpy(dat_resp, CMD_40_RESP, CONFIG_RESP_SIZE);
                 break;
             }
             case 0x41: {
-                compare_dat_resp = true;
                 check_in_config = true;
-                known_command = true;
-                memset(expected_dat_resp, 0, CONFIG_RESP_SIZE);
+                memset(dat_resp, 0, CONFIG_RESP_SIZE);
                 if (mode != MODE_DIGITAL) {
-                    expected_dat_resp[0] = mask[0];
-                    expected_dat_resp[1] = mask[1];
-                    expected_dat_resp[2] = mask[2];
-                    expected_dat_resp[5] = 0x5a;
+                    dat_resp[0] = mask[0];
+                    dat_resp[1] = mask[1];
+                    dat_resp[2] = mask[2];
+                    dat_resp[5] = 0x5a;
                 }
                 break;
             }
+
+            case 0x43:
+            // data dependency handled later
+            // fall thru
             case 0x42: {
                 // TODO:
-                known_command = true;
+                memset(dat_resp, 0, sizeof(dat_resp));
                 break;
             }
             case 0x44: {
                 // data bytes read later:
-                compare_dat_resp = true;
                 check_in_config = true;
-                known_command = true;
-                memset(expected_dat_resp, 0, CONFIG_RESP_SIZE);
+                memset(dat_resp, 0, CONFIG_RESP_SIZE);
                 break;
             }
             case 0x45: {
                 check_in_config = true;
-                known_command = true;
-                memcpy(expected_dat_resp, CMD_45_RESP_TEMPLATE, CONFIG_RESP_SIZE);
-                expected_dat_resp[2] = mode != MODE_DIGITAL;
+                memcpy(dat_resp, CMD_45_RESP_TEMPLATE, CONFIG_RESP_SIZE);
+                dat_resp[2] = mode != MODE_DIGITAL;
                 break;
             }
+            case 0x46:
+            case 0x4c: {
+                check_in_config = true;
+                dat_resp[0] = 0;
+                // data dependency handled later
+                break;
+            }
+
+            case 0x47: {
+                check_in_config = true;
+                memcpy(dat_resp, CMD_47_RESP, CONFIG_RESP_SIZE);
+                break;
+            }
+
             case 0x4d: {
                 check_in_config = true;
-                known_command = true;
-                memcpy(expected_dat_resp, vibration_map, CONFIG_RESP_SIZE);
+                memcpy(dat_resp, vibration_map, CONFIG_RESP_SIZE);
                 break;
             }
             case 0x4f: {
                 check_in_config = true;
-                known_command = true;
-                compare_dat_resp = true;
                 new_mode = MODE_DS2_NATIVE;
-                memcpy(expected_dat_resp, CMD_4F_RESP, CONFIG_RESP_SIZE);
+                memcpy(dat_resp, CMD_4F_RESP, CONFIG_RESP_SIZE);
+                break;
+            }
+            default: {
+                dbgprintf("unknown command 0x%02x\r\n", cmd_bytes[1]);
+                memset(dat_resp, 0xff, sizeof(dat_resp));
                 break;
             }
         }
@@ -190,43 +191,26 @@ static void got_byte(size_t index) {
         switch (cmd) {
             case 0x43: {
                 new_in_config = cmd_bytes[3] == 1;
-                known_command = true;
                 break;
             }
             case 0x46: {
-                compare_dat_resp = true;
-                check_in_config = true;
-                known_command = true;
                 if (cmd_bytes[3] == 0)
-                    memcpy(expected_dat_resp, CMD_46_RESP_PAGE0, CONFIG_RESP_SIZE);
+                    memcpy(dat_resp, CMD_46_RESP_PAGE0, CONFIG_RESP_SIZE);
                 else if (cmd_bytes[3] == 1)
-                    memcpy(expected_dat_resp, CMD_46_RESP_PAGE1, CONFIG_RESP_SIZE);
+                    memcpy(dat_resp, CMD_46_RESP_PAGE1, CONFIG_RESP_SIZE);
                 else
                     dbgprintf("unknown cmd 46 page %u\r\n", cmd_bytes[3]);
                 break;
             }
             case 0x4c: {
-                compare_dat_resp = true;
-                check_in_config = true;
-                known_command = true;
                 if (cmd_bytes[3] == 0)
-                    memcpy(expected_dat_resp, CMD_4C_RESP_PAGE0, CONFIG_RESP_SIZE);
+                    memcpy(dat_resp, CMD_4C_RESP_PAGE0, CONFIG_RESP_SIZE);
                 else if (cmd_bytes[3] == 1)
-                    memcpy(expected_dat_resp, CMD_4C_RESP_PAGE1, CONFIG_RESP_SIZE);
+                    memcpy(dat_resp, CMD_4C_RESP_PAGE1, CONFIG_RESP_SIZE);
                 else
                     dbgprintf("unknown cmd 4c page %u\r\n", cmd_bytes[3]);
                 break;
             }
-            case 0x47: {
-                compare_dat_resp = true;
-                check_in_config = true;
-                known_command = true;
-                CHECK(dat_bytes[4], 0x00);
-                memcpy(expected_dat_resp, CMD_47_RESP, CONFIG_RESP_SIZE);
-                break;
-            }
-
-
         }
     }
 
@@ -250,39 +234,8 @@ static void got_byte(size_t index) {
 }
 
 static void transaction_over(size_t len) {
-    if (!known_command) {
-        dbgprintf("unknown command 0x%02x\r\n", cmd_bytes[1]);
-    }
     if (check_in_config && !new_in_config) {
         dbgprintf("expected to be in config mode\r\n");
-    }
-    size_t expected_len = 3;
-    if (in_config) {
-        expected_len = 9;
-    } else if (mode == MODE_DIGITAL) {
-        expected_len = 5;
-    } else if (mode == MODE_ANALOG) {
-        expected_len = 9;
-    } else if (mode == MODE_DS2_NATIVE) {
-        expected_len = 21;
-    }
-    if (len != expected_len) {
-        dbgprintf("len expected %u got %u mode 0x%02x 0x%02x config %u %u\r\n",
-                expected_len, len, mode, new_mode, in_config, new_in_config);
-    }
-    CHECK(dat_bytes[1], in_config ? 0xf3 : mode);
-    if (compare_dat_resp) {
-        if (memcmp(dat_bytes + 3, expected_dat_resp, expected_len - 3)) {
-            dbgprintf("mismatch, expect: ");
-            for (size_t i = 0; i < expected_len - 3; i++) {
-                dbgprintf("%02x", expected_dat_resp[i]);
-            }
-            dbgprintf(" got: ");
-            for (size_t i = 0; i < expected_len - 3; i++) {
-                dbgprintf("%02x", dat_bytes[i + 3]);
-            }
-            dbgprintf("\r\n");
-        }
     }
 
     in_config = new_in_config;
@@ -320,27 +273,64 @@ static void handle_pin_change(void) {
         atn_end_cycles = ccnt_read();
     }
     if (user_page->reset) {
-        dbgprintf("reset\r\n");
+        //dbgprintf("reset\r\n");
         reset_state();
         user_page->reset = false;
     }
 
-    while (1) {
+    size_t expected_len = 3;
+    if (in_config) {
+        expected_len = 9;
+    } else if (mode == MODE_DIGITAL) {
+        expected_len = 5;
+    } else if (mode == MODE_ANALOG) {
+        expected_len = 9;
+    } else if (mode == MODE_DS2_NATIVE) {
+        expected_len = 21;
+    }
+    check_in_config = false;
+
+    for (byte_idx = 0; byte_idx < expected_len; byte_idx++) {
         unsigned cmd_byte = 0;
-        unsigned dat_byte = 0;
+        unsigned dat_byte;
+
+        switch (byte_idx) {
+            case 0: dat_byte = 0xff; break;
+            case 1: dat_byte = in_config ? 0xf3 : mode; break;
+            case 2: dat_byte = 0x5a; break;
+            default: dat_byte = dat_resp[byte_idx - 3];
+        }
+#ifndef SNIFF
+        dat_bytes[byte_idx] = dat_byte;
+#endif
+
         for (size_t bit = 0; bit < 8; bit++) {
             // Wait for falling edge on CLK
             while ((pins = readl(gplev0)) & PIN_CLK) {
                 if (pins & PIN_ATN) {
-                    dbgprintf("ERR: ATN went high early 1 (byte %zu bit %zu cmd 0x%02x dat 0x%02x)\r\n", byte_idx, bit, cmd_byte, dat_byte);
+                    // very spammy
+                    //dbgprintf("ERR: ATN went high early 1 (byte %zu bit %zu cmd 0x%02x dat 0x%02x)\r\n", byte_idx, bit, cmd_byte, dat_byte);
                     goto out;
                 }
+#ifdef SNIFF
                 if (!(pins & PIN_ACK)) {
                     dbgprintf("ERR: ACK went low early 1 (byte %zu bit %zu cmd 0x%02x dat 0x%02x)\r\n", byte_idx, bit, cmd_byte, dat_byte);
                     goto out;
                 }
+#endif
             }
-            // Here we would switch output
+            if (dat_byte & 1) {
+                // Set input
+                uint32_t fsel = readl(gpio_base + GPIO_GPFSEL0);
+                fsel &= ~FSEL_DAT_MASK;
+                writel(fsel, gpio_base + GPIO_GPFSEL0);
+            } else {
+                // Set output
+                uint32_t fsel = readl(gpio_base + GPIO_GPFSEL0);
+                fsel &= ~FSEL_DAT_MASK;
+                fsel |= FSEL_DAT_OUT;
+                writel(fsel, gpio_base + GPIO_GPFSEL0);
+            }
 
             // Wait for rising edge on CLK
             while (!((pins = readl(gplev0)) & PIN_CLK)) {
@@ -348,23 +338,30 @@ static void handle_pin_change(void) {
                     dbgprintf("ERR: ATN went high early 2 (byte %zu bit %zu cmd 0x%02x dat 0x%02x)\r\n", byte_idx, bit, cmd_byte, dat_byte);
                     goto out;
                 }
+#ifdef SNIFF
                 if (!(pins & PIN_ACK)) {
                     dbgprintf("ERR: ACK went low early 2 (byte %zu bit %zu cmd 0x%02x dat 0x%02x)\r\n", byte_idx, bit, cmd_byte, dat_byte);
                     goto out;
                 }
+#endif
             }
             cmd_byte >>= 1;
             dat_byte >>= 1;
             if (pins & PIN_CMD)
                 cmd_byte |= 0x80;
+
+#ifdef SNIFF
             if (pins & PIN_DAT)
-                dat_byte |= 0x80;
+               dat_byte |= 0x80;
+#endif
         }
+#ifdef SNIFF
         dat_bytes[byte_idx] = dat_byte;
+#endif
         cmd_bytes[byte_idx] = cmd_byte;
         got_byte(byte_idx);
-        byte_idx++;
 
+#ifdef SNIFF
         ended_with_ack = false;
         // Wait for ACK to toggle or ATN go high
         while ((pins = readl(gplev0)) & PIN_ACK && !(pins & PIN_ATN)) {
@@ -378,14 +375,39 @@ static void handle_pin_change(void) {
 
         if (pins & PIN_ATN)
             break;
+#else
+    // Don't ack last byte
+    if (byte_idx == expected_len - 1)
+        break;
+
+    uint32_t timer_start = ccnt_read();
+    // Give some time before responding with ack
+    while (ccnt_read() - timer_start < 2000)
+        ;
+
+    // Set ack output
+    uint32_t fsel = readl(gpio_base + GPIO_GPFSEL0);
+    fsel &= ~FSEL_ACK_MASK;
+    fsel |= FSEL_ACK_OUT;
+    writel(fsel, gpio_base + GPIO_GPFSEL0);
+
+    // Hold ack low for a bit
+    while (ccnt_read() - timer_start < 4000)
+        ;
+
+    // Set ack back to input
+    fsel &= ~FSEL_ACK_MASK;
+    writel(fsel, gpio_base + GPIO_GPFSEL0);
+#endif
     }
 
+out:
     transaction_over(byte_idx);
 
     if (atn_initially_hi) {
         dbgprintf("atn hi for %u cycles\r\n", atn_end_cycles - atn_start_cycles);
     }
-#if 0
+#if 1
     dbgprintf("CMD: ");
     for (size_t i = 0; i < byte_idx; i++) {
         dbgprintf("%02x", cmd_bytes[i]);
@@ -401,8 +423,15 @@ static void handle_pin_change(void) {
     dbgprintf("%u %u %zu %02x %02x %c\r\n", prev_fiq_end - prev_fiq_start, fiq_start - prev_fiq_end, byte_idx, cmd_bytes[1], dat_bytes[1], ended_with_ack ? '+' : '-');
 #endif
 
+    // reset pin states just in case
+    {
+        uint32_t fsel = readl(gpio_base + GPIO_GPFSEL0);
+        fsel &= ~FSEL_ACK_MASK;
+        fsel &= ~FSEL_DAT_MASK;
+        writel(fsel, gpio_base + GPIO_GPFSEL0);
+    }
+
     // ack interrupts
-out:
     {
         uint32_t events = readl(gpio_base + GPIO_GPEDS0);
         writel(events, gpio_base + GPIO_GPEDS0);
@@ -463,6 +492,17 @@ int init_module() {
     if (!gpio_base) {
         pr_err("Couldn't ioremap gpio\n");
         return -ENODEV;
+    }
+
+    {
+        // Ensure inputs
+        uint32_t fsel = readl(gpio_base + GPIO_GPFSEL0);
+        fsel &= ~FSEL_ACK_MASK;
+        fsel &= ~FSEL_DAT_MASK;
+        writel(fsel, gpio_base + GPIO_GPFSEL0);
+
+        // Ensure low state when switched to outputs
+        writel(PIN_ACK | PIN_DAT, gpio_base + GPIO_GPCLR0);
     }
 
     dbgprintf("hi! uart: 0x%08x gpio: 0x%08x", uart_base, gpio_base);
